86d7b57036ccb2a0615154bec953ea643a5700a4
Patrick Lorran Menezes <patrickalmenezes@gmail.com>
2023-11-02 19:33:24 -0300

    trabalhando na estrutura do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ItemComponent } from './components/item/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', redirectTo:'/home', pathMatch:'full'
+{path:'home', component:HomeComponent},
+{path:'item', component: ItemComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { ItemComponent } from './components/item/item/item.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    ItemComponent,
+    HomeComponent,
+    ContactComponent,
+
   ],
   imports: [
     BrowserModule,
